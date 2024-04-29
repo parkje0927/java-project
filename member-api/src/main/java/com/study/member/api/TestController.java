978a8ec5f82a5b2311ea745ee65014f3b94033fe
parkje0927 <parkje09270@gmail.com>
2024-04-29 17:40:57 +0900

    feat: module 패키지 경로 수정

--- member-api/src/main/java/com/study/member/api/TestController.java
+++ member-api/src/main/java/com/study/member/api/TestController.java
@@ -1,6 +1,9 @@
 package com.study.memberapi.api;
 
 import com.study.common.holder.UserIdHolder;
+import com.study.common.wrapper.ResponseResult;
+import com.study.memberapi.data.response.TestResponse;
+import com.study.memberapi.feign.AuthFeignClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TestController {
 
+    private final AuthFeignClient authFeignClient;
+
     @GetMapping("/test")
-    public void test() {
+    public ResponseResult<TestResponse> test() {
         Long userId = UserIdHolder.getUserId();
         System.out.println("userId = " + userId);
+        return ResponseResult.from(authFeignClient.test().result());
     }
 }
